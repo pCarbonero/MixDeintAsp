4a21aafc61e6c1af2c26a3fb31598ddb673fd664
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-24 19:51:57 +0100

    [upd] cosillas

---
+++ CrudsMauiAsp/CrudMaui/ViewModels/clsEditarPersonaVM.cs
@@ -0,0 +1,21 @@
+﻿using BL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudMaui.ViewModels
+{
+    public class clsEditarPersonaVM
+    {
+        public clsPersona PersonaEditada { get; set; }
+
+        public clsEditarPersonaVM() { }
+        public clsEditarPersonaVM(int id) 
+        { 
+            PersonaEditada = clsListadosBL.getPersonaIdBL(id);
+        } 
+    }
+}
--- CrudsMauiAsp/CrudMaui/ViewModels/clsListadoPersonasVM.cs
+++ CrudsMauiAsp/CrudMaui/ViewModels/clsListadoPersonasVM.cs
@@ -1,19 +1,77 @@
-﻿using BL;
+﻿using CrudMaui.ViewModels.Utilidades;
+using BL;
 using Entidades;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrudMaui.ViewModels
 {
-    public class clsListadoPersonasVM
+    public class clsListadoPersonasVM: INotifyPropertyChanged
     {
-        public ObservableCollection<clsPersona> ListadoPersonas { get; }
-        public clsPersona PersonaSeleccionada { get; set; }
+        #region atributos
+        private clsPersona personaSeleccionada;
 
-        public clsListadoPersonasVM() { ListadoPersonas = new ObservableCollection<clsPersona>(clsListadosBL.listadoCompletoPersonasBL()); }
+        private DelegateCommand editarCommand;
+        #endregion
+
+        #region propiedades
+        public ObservableCollection<clsPersona> ListadoPersonas { get; }
+        public clsPersona PersonaSeleccionada 
+        {
+            get { return personaSeleccionada; }
+            set { personaSeleccionada = value; NotifyPropertyChanged("PersonaSeleccionada"); editarCommand.RaiseCanExecuteChanged(); }
+        }
+        public DelegateCommand EditarCommand
+        {
+            get { return editarCommand; }
+        }
+        #endregion
+
+        #region constructores
+        public clsListadoPersonasVM() 
+        { 
+            ListadoPersonas = new ObservableCollection<clsPersona>(clsListadosBL.listadoCompletoPersonasBL());
+            editarCommand = new DelegateCommand(editarCommandExecuted, editarCommandCanExecute);
+        }
+        #endregion
+
+        #region Comandos
+        event EventHandler CanExecuteChanged;
+        public async void editarCommandExecuted()
+        {
+            await Shell.Current.GoToAsync($"///editar?id={personaSeleccionada.Id}");
+        }
+
+        public bool editarCommandCanExecute()
+        {
+            bool canExecute = false;
+
+            if (personaSeleccionada != null)
+            {
+                canExecute = true;
+            }
+
+            return canExecute;
+        }
+        #endregion
+
+        #region Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+
+        {
+
+            PropertyChanged?.Invoke(this, new
+            PropertyChangedEventArgs(propertyName));
+
+        }
+        #endregion
     }
 }
